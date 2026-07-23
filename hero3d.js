6bd88abff34c62318a88927872054d71b66f1606
Rajat <rajatkrishnan321@gmail.com>
2026-07-24 01:59:26 +0530

    Revert "Reposition hero model away from text"
    This reverts commit 342ba7ba842d51df9d74a86e43b8fae668f4497a.

--- hero3d.js
+++ hero3d.js
@@ -249,9 +249,9 @@ if (canvas && hero) {
     camera.updateProjectionMatrix();
 
     const mobile = width < 760;
-    rig.position.set(mobile ? 0.78 : 3.72, mobile ? -1.38 : -0.72, mobile ? -1.02 : -0.5);
-    rig.scale.setScalar(mobile ? 0.3 : 0.42);
-    particles.position.x = mobile ? 0.28 : 1.56;
+    rig.position.set(mobile ? 0.38 : 1.55, mobile ? -1.22 : -0.05, mobile ? -0.85 : -0.3);
+    rig.scale.setScalar(mobile ? 0.54 : 0.86);
+    particles.position.x = mobile ? 0 : 0.8;
     camera.position.z = mobile ? 9.4 : 8.2;
   };
 
